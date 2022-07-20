6a04a2e7506dd9bf4d1d810d59b28292a4adc881
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2022-07-20 07:04:15 +0200

    ui update 3

--- src/components/BordersPage.js
+++ src/components/BordersPage.js
@@ -26,72 +26,74 @@ function Borders(props) {
   return (
     <main className="bg-[#202c37] min-h-screen w-screen text-white">
       <NavBar></NavBar>
-      <Link to="/" className="">
-        <BackButton></BackButton>
-      </Link>
-      <div className="container mt-10 pr-7 pl-7">
-        <div className="flag-box border h-[14rem] w-full">
-          <img
-            className="rounded-lg h-[15rem] w-[20.5rem]"
-            src={selectedBoundaryNew.flags.png}
-            alt="new"
-          />
-        </div>
-        <div className="details-box h-fit w-full mt-10">
-          <h1 className="font-bold text-2xl">
-            {selectedBoundaryNew.name.common}
-          </h1>
-          <div className="innerdetails mt-5">
-            <h3 className="mb-2">
-              Native Name : {selectedBoundaryNew.name.official}
-            </h3>
-            <h3 className="mb-2">
-              Population :{" "}
-              {selectedBoundaryNew.population
-                .toString()
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-            </h3>
-            <h3 className="mb-2">Region : {selectedBoundaryNew.region}</h3>
-            <h3 className="mb-2">
-              Sub-Region : {selectedBoundaryNew.subregion}
-            </h3>
-            <h3 className="mb-2">Capital : {selectedBoundaryNew.capital} </h3>
-            <div className="secondarydetails mt-10">
+      <div className="">
+        <Link to="/" className="">
+          <BackButton></BackButton>
+        </Link>
+        <div className="container mt-10 pr-7 pl-7">
+          <div className="flag-box border h-[14rem] w-full">
+            <img
+              className="rounded-lg h-[15rem] w-[20.5rem]"
+              src={selectedBoundaryNew.flags.png}
+              alt="new"
+            />
+          </div>
+          <div className="details-box h-fit w-full mt-10">
+            <h1 className="font-bold text-2xl">
+              {selectedBoundaryNew.name.common}
+            </h1>
+            <div className="innerdetails mt-5">
               <h3 className="mb-2">
-                Top-Level Domain : {selectedBoundaryNew.tld}{" "}
+                Native Name : {selectedBoundaryNew.name.official}
               </h3>
               <h3 className="mb-2">
-                Currencies :{" "}
-                {Object.values(selectedBoundaryNew.currencies)[0].name}{" "}
+                Population :{" "}
+                {selectedBoundaryNew.population
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
               </h3>
+              <h3 className="mb-2">Region : {selectedBoundaryNew.region}</h3>
               <h3 className="mb-2">
-                Languages :{" "}
-                {Object.values(selectedBoundaryNew.languages).join(" , ")}{" "}
+                Sub-Region : {selectedBoundaryNew.subregion}
               </h3>
+              <h3 className="mb-2">Capital : {selectedBoundaryNew.capital} </h3>
+              <div className="secondarydetails mt-10">
+                <h3 className="mb-2">
+                  Top-Level Domain : {selectedBoundaryNew.tld}{" "}
+                </h3>
+                <h3 className="mb-2">
+                  Currencies :{" "}
+                  {Object.values(selectedBoundaryNew.currencies)[0].name}{" "}
+                </h3>
+                <h3 className="mb-2">
+                  Languages :{" "}
+                  {Object.values(selectedBoundaryNew.languages).join(" , ")}{" "}
+                </h3>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="border-countriesbox mt-10">
-          <h1 className="text-lg">Borders Countries :</h1>
-          <div className="border-container border h-fit w-full flex flex-row flex-wrap mt-4 space-x-1">
-            {selectedBoundaryNew.borders ? (
-              selectedBoundaryNew.borders.map((items) => (
-                <Link key={uuidv4()} to={`${innerBorderVal}`}>
-                  <button
-                    key={uuidv4()}
-                    value={items}
-                    onClick={innerBorderButtonHandler}
-                    className="p-2 mb-8 px-6 bg-[#2b3945] rounded-[0.4rem]"
-                  >
-                    {items}
-                  </button>
-                </Link>
-              ))
-            ) : (
-              <div className="p-2 mb-8 px-6 bg-[#2b3945] rounded-[0.4rem]">
-                No Borders Availaible
-              </div>
-            )}
+          <div className="border-countriesbox mt-10">
+            <h1 className="text-lg">Borders Countries :</h1>
+            <div className="border-container border h-fit w-full flex flex-row flex-wrap mt-4 space-x-1">
+              {selectedBoundaryNew.borders ? (
+                selectedBoundaryNew.borders.map((items) => (
+                  <Link key={uuidv4()} to={`${innerBorderVal}`}>
+                    <button
+                      key={uuidv4()}
+                      value={items}
+                      onClick={innerBorderButtonHandler}
+                      className="p-2 mb-8 px-6 bg-[#2b3945] rounded-[0.4rem]"
+                    >
+                      {items}
+                    </button>
+                  </Link>
+                ))
+              ) : (
+                <div className="p-2 mb-8 px-6 bg-[#2b3945] rounded-[0.4rem]">
+                  No Borders Availaible
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
